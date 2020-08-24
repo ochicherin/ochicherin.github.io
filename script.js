97a832667cd0d310a6db5542e8627b1ec37f5820
Чичерин Олег Николаевич <ochicherin@corp.local>
2020-08-24 19:41:59 +0300

    img

--- script.js
+++ script.js
@@ -516,7 +516,7 @@ function sumImgPages(list) {
   var xml = "";
   var content = "";
   for (var i=0;i<list.options.length;i++) {
-    alert("V 8.0");
+    alert("V 9.0");
     xml = loadXML(list.options[i].value);
     alert(xml);
     content += MakeXSLT(xml,'/img_select.xslt');
